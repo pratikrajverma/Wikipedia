316650de22e66ce060c96692e686a538c2bc852d
pratik <erpratikrajverma@gmail.com>
2024-01-26 14:23:27 +0530

    wiki-pedia clone

--- index.js
+++ index.js
@@ -28,7 +28,33 @@ gratersymbol2.addEventListener("click", ()=>{
         gratersymbol2.style.transform="rotate(90deg)";
     }else{
         gratersymbol2.style.border="";
-        gratersymbol2.style.transform="rotate(-0deg)";
+        gratersymbol2.style.transform="rotate(0deg)";
     }
 })
 gratersymbol2.classList.add("rotate-transition");
+
+
+const maincontentpage = document.querySelector(".main-content-page");
+const maincontentpage2 = document.querySelector(".main-content-page2");
+
+const articlebutton = document.querySelector(".article-button");
+const talkbutton = document.querySelector(".talk-button");
+
+maincontentpage2.classList.add("main-content-page-hide");
+
+
+talkbutton.addEventListener("click",(event)=>{
+    event.preventDefault(); 
+    maincontentpage.classList.add("main-content-page-hide");
+    // maincontentpage2.classList.add("main-content-page-show"); 
+    maincontentpage2.classList.remove("main-content-page-hide"); 
+    // maincontentpage.classList.remove("main-content-page-show");
+}) 
+
+articlebutton.addEventListener("click",(event)=>{
+    event.preventDefault(); 
+    maincontentpage2.classList.add("main-content-page-hide"); 
+    // maincontentpage2.classList.remove("main-content-page-show");
+    // maincontentpage.classList.add("main-content-page-show");
+    maincontentpage.classList.remove("main-content-page-hide");
+})
